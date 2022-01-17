62bf02194f0dadd59ab1e71a4cb3f904646a2daa
Hcarmi <henri.carmigniani@gmail.com>
2022-01-17 14:01:44 +0100

    Création class Animal

--- src/main/java/com/inti/entities/Animal.java
+++ src/main/java/com/inti/entities/Animal.java
@@ -1,5 +1,40 @@
 package com.inti.entities;
 
-public class Animal {
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+public class Animal {
+	
+	private Long idAnimal;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="id_ferme")
+	private Ferme ferme;
+
+	public Long getIdAnimal() {
+		return idAnimal;
+	}
+
+	public void setIdAnimal(Long idAnimal) {
+		this.idAnimal = idAnimal;
+	}
+
+	public Ferme getFerme() {
+		return ferme;
+	}
+
+	public void setFerme(Ferme ferme) {
+		this.ferme = ferme;
+	}
+
+	public Animal() {
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [idAnimal=" + idAnimal + ", ferme=" + ferme + "]";
+	}
+	
+	
 }
