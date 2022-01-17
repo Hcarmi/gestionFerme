17aeec9bc1c07268c763240c95a9c653157c6c5a
in-tl-025 <in-tl-025@localhost.localdomain>
2022-01-17 14:08:10 +0100

    Class Gerant

--- src/main/java/com/inti/entities/Gerant.java
+++ src/main/java/com/inti/entities/Gerant.java
@@ -1,5 +1,28 @@
 package com.inti.entities;
 
-public class Gerant {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
+@Entity
+public class Gerant {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long idGerant;
+	private String nom;
+	private String prenom;
+	
+	@ManyToMany
+	@JoinTable (name="Gerants_Fermes_Associations", joinColumns=@JoinColumn(name="idGerant"), inverseJoinColumns=@JoinColumn(name="idFerme"))
+	private <Ferme> fermes = new ArrayList<>();
+	
+	public Gerant() {
+		super();
+	}
+	
+	
 }
