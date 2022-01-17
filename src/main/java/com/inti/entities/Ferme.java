fdb221573eca27fb73b897a28c2142f100bfa6e1
root <root@Host-001.(none)>
2022-01-17 16:47:15 +0100

    modification class Ferme

--- src/main/java/com/inti/entities/Ferme.java
+++ src/main/java/com/inti/entities/Ferme.java
@@ -1,5 +1,74 @@
 package com.inti.entities;
 
-public class Ferme {
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class Ferme {
+	private Long idFerme;
+	private String nom;
+	@Temporal(TemporalType.DATE)
+	private Date dateAchat;
+
+	@OneToMany(mappedBy="ferme")
+	Set<Animal> animal = new HashSet<>();
+
+	
+	
+	public Ferme(Long idFerme, String nom, Date dateAchat) {
+		super();
+		this.idFerme = idFerme;
+		this.nom = nom;
+		this.dateAchat = dateAchat;
+	}
+
+	public Ferme() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getIdFerme() {
+		return idFerme;
+	}
+
+	public void setIdFerme(Long idFerme) {
+		this.idFerme = idFerme;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Date getDateAchat() {
+		return dateAchat;
+	}
+
+	public void setDateAchat(Date dateAchat) {
+		this.dateAchat = dateAchat;
+	}
+
+	public Set<Animal> getAnimal() {
+		return animal;
+	}
+
+	public void setAnimal(Set<Animal> animal) {
+		this.animal = animal;
+	}
+
+	@Override
+	public String toString() {
+		return "Ferme [idFerme=" + idFerme + ", nom=" + nom + ", dateAchat=" + dateAchat + "]";
+	}
+
+	
+	
+	
 }
